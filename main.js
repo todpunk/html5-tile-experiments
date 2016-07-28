13e30ef71a0d2992299409b21eb6f89cfa624a34
TodPunk <githubpub@todandlorna.com>
2016-07-27 20:59:55 -0600

    Viewport drawing is now a thing, we snap to bounds for the map as well so no glitching occurs. Next up, collisions for player.

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ var context = gamecanvas.getContext('2d');
 var start = null;
 
 // Game vars
-var FPS = 30;
+var FPS = 60;
 var TileSize = 32;
 var PlayerSpeed = 6; 
 
@@ -18,23 +18,28 @@ function Tile(coordinates, tileType) {
     me.tileType = tileType || 0; // default to wall
     me.image = new Image();
     if (me.tileType == 1) {
-        me.image.src = imagemaps.floor[Math.floor(Math.random() * 3)]
+        me.image.src = imagemaps.floor[Math.floor(Math.random() * 3)];
     }
     else {
         me.image.src = imagemaps.wall[0];
     }
 
     me.draw = function(){
-        context.clearRect(me.position.y, me.position.x, TileSize, TileSize);
+        var offset = camera.getOffset();
+        context.clearRect(me.position.x - offset.x, me.position.y - offset.y, TileSize, TileSize);
         context.fillStyle = "#FFFFFF";
-        context.fillRect(me.position.y, me.position.x, TileSize, TileSize);
-        context.drawImage(me.image, me.position.y, me.position.x, TileSize, TileSize);
+        context.fillRect(me.position.x - offset.x, me.position.y - offset.y, TileSize, TileSize);
+        context.drawImage(me.image, me.position.x - offset.x, me.position.y - offset.y, TileSize, TileSize);
     }
 }
 
 
 var camera = {
-    position: new Point(8*TileSize, 6*TileSize) // a 16 wide by 12 tall viewport
+    position: new Point(8*TileSize, 6*TileSize), // a 16 wide by 12 tall viewport
+    getOffset: function() {
+        var offset = new Point(this.position.x - (8*TileSize), this.position.y - (6*TileSize));
+        return offset;
+    }
 };
 
 function Map(){
@@ -57,7 +62,7 @@ function Map(){
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -73,13 +78,19 @@ function Map(){
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0],
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
     ];
-    me.changed = [];
     me.draw = function() {
-        // Draw the tiles that have changed
-        me.changed.forEach(function(item){
-            drawTile(item);
+        // As map size grows, we'll definitely want to iterate over the viewable tiles instead of ALL tiles
+        map.data.forEach(function(row, rowidx){
+            row.forEach(function(col, colidx){
+                tile = map.data[rowidx][colidx];
+                if (
+                    (tile.position.x > (camera.position.x - (9*TileSize)) || tile.position.x < (camera.position.x + (9*TileSize))) &&
+                    (tile.position.y > (camera.position.y - (7*TileSize)) || tile.position.y < (camera.position.y + (7*TileSize)))
+                ){
+                    tile.draw();
+                }
+            })
         })
-        me.changed = [];
     };
 };
 var map = new Map();
@@ -105,73 +116,39 @@ var Key = {
   }
 };
 
-function drawTile(coordinates){
-    tile = map.data[coordinates.y][coordinates.x]
-    tile.draw();
-}
-
 
 function Player() {
     var me = this;
-    var prevx = 0;
-    var prevy = 0;
-    me.position = new Point(TileSize, TileSize*2),
+    me.position = new Point(TileSize*2, TileSize),
     me.direction = new Image();
     me.speed = PlayerSpeed;
     me.direction.src = imagemaps.player[0],
     me.draw = function() {
-        context.drawImage(me.direction, me.position.y, me.position.x, TileSize, TileSize);
+        var offset = camera.getOffset();
+        context.drawImage(me.direction, me.position.x - offset.x, me.position.y - offset.y, TileSize, TileSize);
     }
     me.moveUp = function() {
-        prevx = Math.floor(me.position.x/TileSize);
-        prevy = Math.floor(me.position.y/TileSize);
-        var point = new Point(prevx,prevy);
-        map.changed.push(point)
-        me.position.x -= me.speed;
-        if(prevx > 0) {
-            map.changed.push(new Point(prevy,prevx-1))
-        }
-        if(prevx < map.data.length) {
-            map.changed.push(new Point(prevy,prevx+1))
+        me.position.y -= me.speed;
+        if (me.position.y < camera.position.y && camera.position.y > (6*TileSize)) {
+            camera.position.y = Math.max(me.position.y, (6*TileSize));
         }
     }
     me.moveDown = function() {
-        prevx = Math.floor(me.position.x/TileSize);
-        prevy = Math.floor(me.position.y/TileSize);
-        var point = new Point(prevx,Math.floor(me.position.y/TileSize));
-        map.changed.push(point)
-        me.position.x += me.speed;
-        if(prevx > 0) {
-            map.changed.push(new Point(prevy,prevx-1))
-        }
-        if(prevx < map.data.length) {
-            map.changed.push(new Point(prevy,prevx+1))
+        me.position.y += me.speed;
+        if (me.position.y > camera.position.y && (camera.position.y + (6*TileSize)) < map.data.length*TileSize) {
+            camera.position.y = Math.min(me.position.y, map.data.length*TileSize - (6*TileSize));
         }
     }
     me.moveLeft = function() {
-        prevx = Math.floor(me.position.x/TileSize);
-        prevy = Math.floor(me.position.y/TileSize);
-        var point = new Point(prevx,prevy);
-        map.changed.push(point)
-        me.position.y -= me.speed;
-        if(prevy > 0) {
-            map.changed.push(new Point(prevy,prevx-1))
-        }
-        if(prevy < map.data[0].length) {
-            map.changed.push(new Point(prevy,prevx+1))
+        me.position.x -= me.speed;
+        if (me.position.x < camera.position.x && camera.position.x > (8*TileSize)) {
+            camera.position.x = Math.max(me.position.x, (8*TileSize));
         }
     }
     me.moveRight = function() {
-        prevx = Math.floor(me.position.x/TileSize);
-        prevy = Math.floor(me.position.y/TileSize);
-        var point = new Point(prevy,prevx);
-        map.changed.push(point)
-        me.position.y += me.speed;
-        if(prevy > 0) {
-            map.changed.push(new Point(prevy,prevx-1))
-        }
-        if(prevy < map.data[0].length) {
-            map.changed.push(new Point(prevy,prevx+1))
+        me.position.x += me.speed;
+        if (me.position.x > camera.position.x && (camera.position.x + (8*TileSize)) < map.data[0].length*TileSize) {
+            camera.position.x = Math.min(me.position.x, map.data[0].length*TileSize - (8*TileSize));
         }
     }
 }
@@ -195,10 +172,6 @@ function update(timestamp) {
         if (Key.isDown(Key.LEFT)) player.moveLeft();
         if (Key.isDown(Key.DOWN)) player.moveDown();
         if (Key.isDown(Key.RIGHT)) player.moveRight();
-        // TODO: Need to redraw the map tiles we've overwritten, mark them somehow
-        //context.clearRect(0, 0, 512, 384);
-        //context.fillStyle = "#FFFFFF";
-        //context.fillRect(0, 0, 512, 384);
         map.draw();
         player.draw();
     }
@@ -206,29 +179,21 @@ function update(timestamp) {
 }
 
 var setup = function() {
-    console.log('preload completed')
-    console.log(new Date())
     // Initialize some stuff here and start the whole process running
     context.clearRect(0, 0, 512, 384);
     context.fillStyle = "#FFFFFF";
     context.fillRect(0, 0, 512, 384);
     map.data.forEach(function(row, rowidx){
         row.forEach(function(col, colidx){
-            var newTile = new Tile(new Point(rowidx*TileSize, colidx*TileSize), col);
-            map.data[rowidx][colidx] = newTile;
-            map.changed.push(new Point(rowidx, colidx));
+            var newTile = new Tile(new Point(colidx*TileSize, rowidx*TileSize), col);
+            (map.data[rowidx])[colidx] = newTile;
         })
     })
-    console.log(map);
     map.draw();
     player.draw();
-    console.log('setup completed')
-    console.log(new Date())
     window.addEventListener('keyup', function(event) { Key.onKeyup(event); }, false);
     window.addEventListener('keydown', function(event) { Key.onKeydown(event); }, false);
     window.requestAnimationFrame(update);
 }
 
-console.log('preload started')
-console.log(new Date())
 preload(setup);
